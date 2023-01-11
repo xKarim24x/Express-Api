1160d2170a8c8fce8171efd7c361df6f48a7d969
Karim <aksharif@umich.edu>
2023-01-11 17:10:54 -0500

    edit

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ app.use(express.json());
 
 app.use('/api/users', apiRouter)
 
+app.get("/", (req, res) => {
+    res.send("Express on vercel")
+})
+
 app.listen(process.env.PORT || '3000', () => {
 
     console.log(`Server is running on port: ${process.env.PORT || '3000'}`);
